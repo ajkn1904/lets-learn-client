af5ce55a16153880459e6135a51ea706d88de013
ajkn1904 <ajknishat@gmail.com>
2022-10-27 19:54:27 +0600

    update

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,18 +3,18 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h1 className="text-4xl text-center font-bold my-14">Welcome to BLOG!</h1>
+            <h1 className="text-4xl text-center font-bold my-14 bg-black">Welcome to BLOG!</h1>
 
             <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
-                <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" alt="" />
+                <img src="https://images.unsplash.com/photo-1596461009523-7d647a4e2399?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" className="max-w-sm rounded-lg shadow-2xl" alt="" />
                 <div className="flex flex-col gap-5">
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
                     <div className="collapse-title text-xl font-bold">
                     what is cors?
                     </div>
                     <div className="collapse-content"> 
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism. It allows a server to indicate any origins other than its own from which a browser should permit loading resources.</p>
                     </div>
                 </div>
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
@@ -22,7 +22,8 @@ const Blog = () => {
                     Why are you using firebase? What other options do you have to implement authentication?
                     </div>
                     <div className="collapse-content"> 
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>Firebase helps us to develop high-quality apps, helps to grow user base, it's easy to set up, fast, in many cases requires only front-end logic. Each feature works independently, and they work even better together. 
+                        We can implement authentication with other options like: Google, Github, Facebook, Twitter etc.</p>
                     </div>
                 </div>
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
@@ -30,7 +31,7 @@ const Blog = () => {
                     How does the private route work?
                     </div>
                     <div className="collapse-content"> 
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>The react private route requires a user being authorized to visit a route.  Mainly this component renders child components if the user is logged in. If not logged in the user is redirected to the login page with the return url passed in the location state property.</p>
                     </div>
                 </div>
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
@@ -38,7 +39,8 @@ const Blog = () => {
                     What is Node? How does Node work?
                     </div>
                     <div className="collapse-content"> 
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>Node allows developers to write JavaScript code that runs directly in a computer process itself instead of in a browser. As an asynchronous event-driven JavaScript runtime, Node.js is designed to build scalable network applications It is an open source server environment, runs on various platforms like Windows, Linux, Mac OS X, etc. 
+                        Node. js runs on chrome v8 engine which converts javascript code into machine code. Node JS Web Server internally maintains a Limited Thread pool to provide services to the Client Requests. Node JS Web Server receives those requests and places them into a Queue. It is known as “Event Queue”. Node JS Web Server internally has a Component, known as “Event Loop”.</p>
                     </div>
                 </div>
                 </div>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <div className='flex justify-center items-center w-full h-20 bg-slate-800 text-white'>
+            <small>© 2022 || Anika Jumana Khanam</small>
+        </div>
+    );
+};
+
+export default Footer;
