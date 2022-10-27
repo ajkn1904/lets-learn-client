e6ff2d4c9cfdfa6a1c3a824f79b7af73d637adb7
ajkn1904 <ajknishat@gmail.com>
2022-10-27 09:18:48 +0600

    update

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
 
 const Checkout = () => {
     const courseDetail = useLoaderData();
-    const {name, img, outcome, price, ratings, outline} = courseDetail;
+    const {name, img, description, price} = courseDetail;
+
+    const handleCheckOut = () => {
+        toast.success("Congratulations!!!")
+    }
     return (
-        <div>
-            <h1>{name}</h1>
+        <div className="card lg:card-side bg-base-100 shadow-xl w-9/12 mx-auto my-14">
+        <figure><img src={img} alt=""/></figure>
+        <div className="card-body">
+            <h2 className="card-title">{name}</h2>
+            <p>{description}</p>
+            <small>Price: ${price}</small>
+            <div className="card-actions justify-end">
+                <button onClick={handleCheckOut} className="btn btn-outline btn-primary font-bold">Checkout Now</button>
+                <Toaster/>
+            </div>
+        </div>
         </div>
     );
 };
