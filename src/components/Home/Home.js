e8efc8b76420c8bcb39002982d679c31afcb349d
ajkn1904 <ajknishat@gmail.com>
2022-12-22 16:41:49 +0600

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ import Feedback from './Feedback';
 const Home = () => {
     return (
         <>
-            <div className="hero min-h-[90vh] bg-cover" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1599666520394-50d845fe09c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")` }}>
+            <div className="hero min-h-[70vh] bg-cover" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1599666520394-50d845fe09c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")` }}>
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
