9b64a0ff8ee68b3eb0acff6a68b585203f75f253
ajkn1904 <ajknishat@gmail.com>
2022-12-22 16:40:02 +0600

    update

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -5,23 +5,23 @@ import CourseSideNav from '../CourseSideNav/CourseSideNav';
 
 const Courses = () => {
     const courses = useLoaderData();
-    
+
     return (
         <div className="flex">
             {/* course content */}
-            <div className='grow'  style={{marginLeft: "15px", marginBottom: "50px"}}>
-                    <p className='text-center font-bold py-6 text-3xl  mt-20 mb-16'>ALL COURSES</p>
-                <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-16 sm:mx-10 md:ml-14 lg:ml-20'>
+            <div className='grow' style={{ marginLeft: "15px", marginBottom: "50px" }}>
+                <p className='text-center font-bold py-6 text-3xl  mt-20 mb-16'>ALL COURSES</p>
+                <div className='md:w-9/12 lg:w-9/12 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 sm:mx-10 md:mx-auto lg:mx-auto'>
                     {courses.map(course => <CourseCards course={course} key={course.id}></CourseCards>)
                     }
                 </div>
             </div>
             {/* side nav of courses page */}
-            <div className="grow-0 bg-blue-50 ml-4">
+            <div className="bg-blue-50 ml-4">
                 <p className='text-primary text-center text-2xl font-semibold h-5 my-10'>FIND COURSES</p>
-            {
-                courses.map(course => <CourseSideNav course={course} key={course.id}></CourseSideNav>)
-            }
+                {
+                    courses.map(course => <CourseSideNav course={course} key={course.id}></CourseSideNav>)
+                }
             </div>
         </div>
     );
